c8021f7f12e570b35754b317ca4309dba9fa7f61
Nazar Petruk <43855599+nazar2206@users.noreply.github.com>
2018-11-24 12:05:50 +0200

    Update app.java

--- src/main/java/com/lits3/app.java
+++ src/main/java/com/lits3/app.java
@@ -5,8 +5,9 @@ import java.util.Map;
 
 public class app {
     public static void main(String[] args) {
-        String sentence = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
         Map<Character, Integer> map = new HashMap<>();
+
+        String sentence = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
         for (int i = 0; i < sentence.length(); i++) {
             if ( map.containsKey(sentence.charAt(i))) {
